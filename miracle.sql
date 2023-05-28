c745423c213f02fadbf019bee49d8c5118201637
queenalpha <oxcroprichvie@gmail.com>
2023-05-28 12:38:38 +0700

    sql baru

--- miracle.sql
+++ miracle.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 24, 2023 at 03:02 PM
+-- Generation Time: May 28, 2023 at 07:38 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -24,6 +24,20 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `akun`
+--
+
+CREATE TABLE `akun` (
+  `ID_akun` int(11) NOT NULL,
+  `nama_akun` varchar(200) NOT NULL,
+  `email_akun` varchar(150) NOT NULL,
+  `pass_akun` varchar(150) NOT NULL,
+  `pict_akun` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `campaign`
 --
 
@@ -35,9 +49,52 @@ CREATE TABLE `campaign` (
   `target` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `donatur`
+--
+
+CREATE TABLE `donatur` (
+  `ID_donatur` int(11) NOT NULL,
+  `nama_donatur` varchar(250) NOT NULL,
+  `email_donatur` varchar(200) NOT NULL,
+  `telp_donatur` varchar(100) NOT NULL,
+  `donasi` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fundraising`
+--
+
+CREATE TABLE `fundraising` (
+  `ID_fundraising` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `management`
+--
+
+CREATE TABLE `management` (
+  `ID_management` int(11) NOT NULL,
+  `email_management` varchar(100) NOT NULL,
+  `pass_management` varchar(150) NOT NULL,
+  `telp_management` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `akun`
+--
+ALTER TABLE `akun`
+  ADD PRIMARY KEY (`ID_akun`);
 
 --
 -- Indexes for table `campaign`
@@ -46,14 +103,56 @@ ALTER TABLE `campaign`
   ADD PRIMARY KEY (`ID_campaign`);
 
 --
+-- Indexes for table `donatur`
+--
+ALTER TABLE `donatur`
+  ADD PRIMARY KEY (`ID_donatur`);
+
+--
+-- Indexes for table `fundraising`
+--
+ALTER TABLE `fundraising`
+  ADD PRIMARY KEY (`ID_fundraising`);
+
+--
+-- Indexes for table `management`
+--
+ALTER TABLE `management`
+  ADD PRIMARY KEY (`ID_management`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `akun`
+--
+ALTER TABLE `akun`
+  MODIFY `ID_akun` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `campaign`
 --
 ALTER TABLE `campaign`
   MODIFY `ID_campaign` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `donatur`
+--
+ALTER TABLE `donatur`
+  MODIFY `ID_donatur` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `fundraising`
+--
+ALTER TABLE `fundraising`
+  MODIFY `ID_fundraising` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `management`
+--
+ALTER TABLE `management`
+  MODIFY `ID_management` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
