96d5cc9eeddad927462a5b29be5b5eabde60f729
Adeteyah <adeteyah@gmail.com>
2023-06-06 13:58:42 +0700

    reset

--- dete/server/conn.php
+++
@@ -1,2 +0,0 @@
-<?php session_start();
-$conn = mysqli_connect("localhost", "root", "", "miracle") or die("Can't connect to the database");
--- server/connection.php
+++
@@ -1,3 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "miracle")
-    or die("Can't connect to the database");
