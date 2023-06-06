b9b4e107808f2c786181ab78d5b22190cd0075cc
Adeteyah <adeteyah@gmail.com>
2023-06-06 14:55:52 +0700

    first entry

---
+++ pages/donasiPageLogged.php
@@ -0,0 +1,58 @@
+<?php $title = "Miracle - Landing Page"; ?>
+<?php include('../server/connection.php');
+
+?>
+<?php include('../components/header.php'); ?>
+
+
+
+<div class="container">
+  <div class="row">
+    <div class="text-center my-4 mb-5">
+      <h2>Give Your Charity For Donation</h2>
+      <p>Be a miracle foreach others</p>
+    </div>
+  </div>
+
+
+  <div class="row align-items-center" id="donasi">
+    <div class="col-12 col-md-12 col-lg-4 mb-5">
+      <div class="card p-1 h-100">
+        <img src="Assets/image/cancer.jpg" class="card-img-top object-fit-cover" width="100%" height="201px" alt="">
+        <div class="card-body">
+          <h5 class="card-tittle">Donasi untuk ariel</h5>
+          <p class="card-text">Ariel, gadis kecil 12 tahun sedang berjuang untuk kesembuhannya</p>
+          <p class="card-text">Membutuhkan Rp500,000</p>
+          <button class="btn-donasi">Donate</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-12 col-md-12 col-lg-4 mb-5">
+      <div class="card p-1 h-100">
+        <img src="Assets/image/cancer.jpg" class="card-img-top object-fit-cover" width="100%" height="201px" alt="">
+        <div class="card-body">
+          <h5 class="card-tittle">Donasi untuk ariel</h5>
+          <p class="card-text">Ariel, gadis kecil 12 tahun sedang berjuang untuk kesembuhannya</p>
+          <p class="card-text">Membutuhkan Rp500,000</p>
+          <button class="btn-donasi">Donate</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-12 col-md-12 col-lg-4 mb-5">
+      <div class="card p-1 h-100">
+        <img src="Assets/image/cancer.jpg" class="card-img-top object-fit-cover" width="100%" height="201px" alt="">
+        <div class="card-body">
+          <h5 class="card-tittle">Donasi untuk ariel</h5>
+          <p class="card-text">Ariel, gadis kecil 12 tahun sedang berjuang untuk kesembuhannya</p>
+          <p class="card-text">Membutuhkan Rp500,000</p>
+          <button class="btn-donasi">Donate</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</section>
+
+
+<?php include('../components/js.php'); ?>
+<?php include('../components/footer.php'); ?>
