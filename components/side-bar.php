5d30ea3f8dc9a39fbda43c65fc519096aebdfc7a
queenalpha <oxcroprichvie@gmail.com>
2023-06-07 22:22:55 +0700

    ts

---
+++ components/side-bar.php
@@ -0,0 +1,27 @@
+
+<div class="side-bar">
+    <div class="side-text">
+        <ul>
+            <li>
+                <a href="">Profile</a>
+            </li>
+            <li>
+                <a href="riwayatDonasi.php">Riwayat Donasi</a>
+            </li>
+            <li>
+                <a href="campaign.php">Program Campaign</a>
+            </li>
+        </ul>
+
+        <div class="link-bottom">
+            <ul>
+                <li>
+                    <a href="index.php">Home</a>
+                </li>
+                <li>
+                    <a href="profilePage.php?logout=1">Logout</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
