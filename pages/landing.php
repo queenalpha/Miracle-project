5564497afa30fe50c9f298bbb533bcaea719cfce
Adeteyah <adeteyah@gmail.com>
2023-06-09 11:33:32 +0700

    1

--- pages/landing.php
+++ pages/landing.php
@@ -1,124 +1,98 @@
-<!-- Konfigurasi -->
+<!-- Model -->
 <?php
 $title = "Miracle - Landing Page";
 $prevent = 'authenticated';
 include('../server/connection.php');
 ?>
-<!-- Logic -->
+<!-- Controller -->
 <?php
 $from_campaign = "SELECT * FROM campaigns ORDER BY campaign_id desc LIMIT 3";
 $result_camp = mysqli_query($conn, $from_campaign);
 ?>
-<!-- Logic -->
+<!-- View -->
 <?php include('../components/header.php'); ?>
 <?php include('../components/navbar.php'); ?>
 <?php include('../components/carousel.php'); ?>
-<main>
-
-    <!-- Home donation -->
-    <section class="open-donation md-5">
-
-        <div class="container">
-
-            <!-- Title -->
-            <div class="text-center my-5">
-                <h2>Open Donation</h2>
-                <small class="text-muted fst-italic">Be a miracle foreach others</small>
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title text-center justify-content-center" id="exampleModalLabel">Ayo berdonasi
+                </h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-
-            <div class="row align-items-center ms-4" id="donasi">
-                <?php while ($row = mysqli_fetch_assoc($result_camp)): ?>
-                    <div class="col-12 col-md-12 col-lg-4 mb-5">
-                        <div class="card p-1 h-100">
-                            <img src="<?php echo $row['campaign_thumbnail'] ?>" class="card-img-top object-fit-cover"
-                                width="100%" height="201px" alt="">
-                            <div class="card-body">
-                                <h5 class="card-tittle">
-                                    <?php echo $row['campaign_name'] ?>
-                                </h5>
-                                <p class="card-text">
-                                    <?php echo $row['campaign_description'] ?>
-                                </p>
-                                <p class="card-text">Membutuhkan Rp
-                                    <?php echo number_format($row['campaign_target']) ?>
-                                </p>
-                                <button type="button" class="btn-donasi" data-bs-toggle="modal"
-                                    data-bs-target="#exampleModal">
-                                    Donate
-                                </button>
-
-                                <!-- Modal Donate Data Donatur -->
-                                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
-                                    aria-hidden="true">
-                                    <div class="modal-dialog">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title text-center justify-content-center"
-                                                    id="exampleModalLabel">Ayo berdonasi
-                                                </h5>
-                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                    aria-label="Close"></button>
-                                            </div>
-                                            <div class="modal-body">
-                                                <form method="POST" enctype="multipart/form-data" action="#">
-                                                    <div class="form-group row">
-                                                        <label for="colFormLabelSm"
-                                                            class="col-sm-20 col-form-label col-form-label-sm">Nama</label>
-                                                        <div class="col-sm-20">
-                                                            <input type="text" class="form-control form-control-sm"
-                                                                id="colFormLabelSm" name="judul" placeholder="Masukan nama">
-                                                        </div>
-                                                        <label for="colFormLabelSm"
-                                                            class="col-sm-20 col-form-label col-form-label-sm">Email</label>
-                                                        <div class="col-sm-20">
-                                                            <input type="text" class="form-control form-control-sm"
-                                                                id="colFormLabelSm" name="harga"
-                                                                placeholder="Masukan Email">
-                                                        </div>
-                                                        <label for="colFormLabelSm"
-                                                            class="col-sm-20 col-form-label col-form-label-sm">Telphone</label>
-                                                        <div class="col-sm-20 mb-2">
-                                                            <input type="text" class="form-control form-control-sm"
-                                                                id="colFormLabelSm" name="harga"
-                                                                placeholder="Masukan Telephone">
-                                                        </div>
-                                                    </div>
-                                                    <div class="modal-footer">
-                                                        <input type="submit" class="btn-donasi mt-3" data-bs-toggle="modal"
-                                                            data-bs-target="#transaksi" value="Save">
-                                                    </div>
-                                                </form>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+            <div class="modal-body">
+                <form method="POST" enctype="multipart/form-data" action="#">
+                    <div class="form-group row">
+                        <label for="colFormLabelSm" class="col-sm-20 col-form-label col-form-label-sm">Nama</label>
+                        <div class="col-sm-20">
+                            <input type="text" class="form-control form-control-sm" id="colFormLabelSm" name="judul"
+                                placeholder="Masukan nama">
+                        </div>
+                        <label for="colFormLabelSm" class="col-sm-20 col-form-label col-form-label-sm">Email</label>
+                        <div class="col-sm-20">
+                            <input type="text" class="form-control form-control-sm" id="colFormLabelSm" name="harga"
+                                placeholder="Masukan Email">
+                        </div>
+                        <label for="colFormLabelSm" class="col-sm-20 col-form-label col-form-label-sm">Telphone</label>
+                        <div class="col-sm-20 mb-2">
+                            <input type="text" class="form-control form-control-sm" id="colFormLabelSm" name="harga"
+                                placeholder="Masukan Telephone">
                         </div>
                     </div>
-                <?php endwhile; ?>
+                    <div class="modal-footer">
+                        <input type="submit" class="btn-donasi mt-3" data-bs-toggle="modal" data-bs-target="#transaksi"
+                            value="Save">
+                    </div>
+                </form>
             </div>
-    </section>
-
-    <!-- advertise -->
-    <section class="advertise">
-        <div class="container">
-            <video width="100%" class="video rounded-4" autoplay="true" loop="true">
-                <source src="../assets/videoAdvertise.mov" type="video/mp4">
-            </video>
+        </div>
+    </div>
+</div>
+<main>
+    <section class="container">
+        <div class="text-center my-5">
+            <h2>Open Donation</h2>
+            <small class="text-muted fst-italic">Be a miracle foreach others</small>
+        </div>
+        <div class="row align-items-center ms-4" id="donasi">
+            <?php while ($row = mysqli_fetch_assoc($result_camp)): ?>
+                <div class="col-12 col-md-12 col-lg-4 mb-5">
+                    <div class="card p-1 h-100">
+                        <img src="<?php echo $row['campaign_thumbnail'] ?>" class="card-img-top object-fit-cover"
+                            width="100%" height="201px" alt="">
+                        <div class="card-body">
+                            <h5 class="card-tittle">
+                                <?php echo $row['campaign_name'] ?>
+                            </h5>
+                            <p class="card-text">
+                                <?php echo $row['campaign_description'] ?>
+                            </p>
+                            <p class="card-text">Membutuhkan Rp
+                                <?php echo number_format($row['campaign_target']) ?>
+                            </p>
+                            <button type="button" class="btn-donasi" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                Donate
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            <?php endwhile; ?>
         </div>
     </section>
-
-
-    <!-- Lead/Inviting -->
-    <div class="container-fluid py-5 text-center" style="background-color: #e5ba73;">
+    <section class="container">
+        <video class="w-100 rounded-4 my-5" autoplay="true" loop="true">
+            <source src="../assets/videoAdvertise.mov" type="video/mp4">
+        </video>
+    </section>
+    <section class="container-fluid py-5 text-center" style="background-color: #e5ba73;">
         <div class="my-5">
             <h4>Make your life useful to others</h4>
             <p>Create a miracle for someone who still wants to keep fighting</p>
             <a class="btn btn-primary" href="#">Donation</a>
             <a class="btn btn-primary" href="#">Buat Campaign</a>
         </div>
-    </div>
-    <!-- Section -->
+    </section>
     <div class="container my-5">
         <div class="row">
             <div class="col">
