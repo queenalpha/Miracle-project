0cf8358fcf5e347da7ac67867aeeb440eba62f23
Adeteyah <adeteyah@gmail.com>
2023-06-11 15:23:40 +0700

    1

--- pages/view.php
+++ pages/view.php
@@ -42,7 +42,12 @@ if (isset($_GET['campaign']) && isset($_GET['delete'])) {
 }
 if (isset($_GET['donation']) && !empty($_GET['donation'])) {
     $target = $_GET['donation'];
-    $query = "SELECT * FROM donations WHERE donation_id = $target";
+    $query = "SELECT donations.donation_id, donations.donation_date, accounts.account_name, accounts.account_avatar, campaigns.campaign_name, campaigns.campaign_thumbnail, donations.donation_amount
+    FROM donations
+    JOIN accounts ON accounts.account_id = donations.donation_account
+    JOIN campaigns ON campaigns.campaign_id = donations.donation_campaign
+    WHERE donations.donation_id = '$target'
+    ORDER BY donations.donation_date DESC";
     $result = mysqli_query($conn, $query);
 }
 if (isset($_POST['edit-account'])) {
@@ -140,13 +145,16 @@ if (isset($_POST['edit-campaign'])) {
                                                     type="file" name="thumbnail" accept="image/*" required>
                                             </div>
                                             <div class="col col-12">
-                                                <label for="f-6" class="form-label">Tipe Role</label>
-                                                <select name="level" id="f-6" class="form-select"
-                                                    aria-label="Default select example" <?php if (!isset($_GET['editable'])) {
-                                                        echo 'disabled';
-                                                    } ?>>
-                                                    <option value="fundraiser">Fundraiser</option>
-                                                    <option value="admin">Admin</option>
+                                                <label for="f-6" class="form-label">Role</label>
+                                                <select id="f-6" name="level" class="form-select" <?php if (!isset($_GET['editable'])) {
+                                                    echo 'disabled';
+                                                } ?>>
+                                                    <option value="fundraiser" <?php if ($row['account_level'] == 'fundraiser') {
+                                                        echo 'selected';
+                                                    } ?>>Fundraiser</option>
+                                                    <option value="admin" <?php if ($row['account_level'] == 'admin') {
+                                                        echo 'selected';
+                                                    } ?>>Admin</option>
                                                 </select>
                                             </div>
                                             <div class="col col-12 col-md-6">
@@ -321,6 +329,55 @@ if (isset($_POST['edit-campaign'])) {
                 <?php
                 if (isset($_GET['donation']) && !empty($_GET['donation'])) {
                     while ($row = mysqli_fetch_assoc($result)): ?>
+                        <div id="printarea" class="card">
+                            <h5 class="card-header">Donation Receipt</h5>
+                            <div class="card-body">
+                                <img class="img-thumbnail mb-3 w-25" src="../assets/icon/typograph.png" alt="Miracle">
+                                <div class="row">
+                                    <p class="my-0">PT. Miracle Indonesia</p>
+                                    <small class="mb-5">#TemanBaik</small>
+                                </div>
+                                <div class="row">
+                                    <div class="col">
+                                        <p class="m-0 fs-5">Donatur</p>
+                                        <p class="m-0 fs-1">
+                                            <?= $row['account_name'] ?>
+                                        </p>
+                                    </div>
+                                    <div class="col">
+                                        <p class="m-0 fs-5">Untuk Kampanye</p>
+                                        <p class="m-0 fs-1">
+                                            <?= $row['campaign_name'] ?>
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="row mt-3">
+                                    <div class="col">
+                                        <p class="m-0 fs-5">Tanggal</p>
+                                        <p class="m-0 fs-1">
+                                            <?= date("d F Y", strtotime($row['donation_date'])); ?>
+                                        </p>
+                                    </div>
+                                    <div class="col">
+                                        <p class="m-0 fs-5">Sebesar</p>
+                                        <p class="m-0 fs-1">
+                                            <?= "Rp." .
+                                                number_format($row['donation_amount'])
+                                                . ",-" ?>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                Donation ID:
+                                <?= $row['donation_id'] ?>
+                            </div>
+                        </div>
+                        <div class="card mt-3">
+                            <input class="btn btn-primary mt-3" type='button' id='btn' value='Print' onclick='window.print();'>
+                            <a class="btn btn-secondary mt-3" href="../pages/manage.php?manage=donations">Kembali Ke Histori
+                                Donasi</a>
+                        </div>
                     <?php endwhile;
                 } ?>
             </div>
