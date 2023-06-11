ff6cccbcb6bff318a283da8c2c41ade7080645e7
queenalpha <oxcroprichvie@gmail.com>
2023-06-11 16:52:54 +0700

    12

--- components/navbar.php
+++ components/navbar.php
@@ -38,7 +38,7 @@
                 <?php
                 } else {?>
                      <li class="nav-item">
-                        <a class="nav-link" href="#">Riwayat Donasi</a>
+                        <a class="nav-link" href="history.php">Riwayat Donasi</a>
                     </li>
                 <?php }?>
                 </ul>
