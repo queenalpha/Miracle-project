667cbe47cc0e24ef5b315a0a82577c6446cd1b70
queenalpha <oxcroprichvie@gmail.com>
2023-06-11 18:55:32 +0700

    1

--- pages/profile.php
+++ pages/profile.php
@@ -56,7 +56,7 @@ include('../server/connection.php');
 <!-- UI -->
 <link rel="stylesheet" href="../Assets/css/profile.css">
 <?php include('../components/header.php'); ?>
-<!-- <?php include('../components/sidebar.php');?> -->
+<?php include('../components/sidebar.php');?>
 
 <div class="container profile">
     <div class="profile-content d-flex">
